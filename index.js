5c6342ab5a3daae8abd9170258dc874d5e2937ed
Steve Thompson <writetome51@gmail.com>
2019-08-09 12:09:02 -0600

    Got rid of unnecessary dependency

--- index.js
+++ index.js
@@ -1,9 +1,8 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", {value: true});
-var isArray_notArray_1 = require("@writetome51/is-array-not-array");
 
 
 function errorIfNotArray(arg) {
-	if (isArray_notArray_1.notArray(arg)) throw new Error('Input must be array.');
+	if (!(Array.isArray(arg))) throw new Error('Input must be array.');
 }
 exports.errorIfNotArray = errorIfNotArray;
